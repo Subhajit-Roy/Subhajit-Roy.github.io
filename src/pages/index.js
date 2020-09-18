4a17eb3dba43821d5a6bac2f85844254483b25f6
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2020-09-18 16:04:39 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Wave from 'react-wavify';
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -21,6 +23,17 @@ const IndexPage = () => (
     <br/>
     <p>Plese just work please</p>
     <p>I don't know </p>
+    <Wave mask="url(#mask)" fill="#1277b0" >
+  <defs>
+    <linearGradient id="gradient" gradientTransform="rotate(90)">
+      <stop offset="0" stopColor="white" />
+      <stop offset="0.5" stopColor="black" />
+    </linearGradient>
+    <mask id="mask">
+      <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)"  />
+    </mask>
+  </defs>
+</Wave>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
