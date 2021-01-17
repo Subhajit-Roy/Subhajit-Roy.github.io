dbc567a3a057ca4373a5ba86e651e116c71827d1
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2021-01-17 17:11:15 +0530

    window modification

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,41 +13,41 @@ import "./index.css"
 
 
 export default class IndexPage extends Component{
-  constructor() {
-    super();
-    this.state = {
-      width:  800,
-      height: 182
-    }
-  }
+//   constructor() {
+//     super();
+//     this.state = {
+//       width:  800,
+//       height: 182
+//     }
+//   }
 
-  /**
-   * Calculate & Update state of new dimensions
-   */
-  updateDimensions() {
-    if(window.innerWidth < 500) {
-      this.setState({ width: 450, height: 102 });
-    } else {
-      let update_width  = window.innerWidth-100;
-      let update_height = Math.round(update_width/4.4);
-      this.setState({ width: update_width, height: update_height });
-    }
-  }
+//   /**
+//    * Calculate & Update state of new dimensions
+//    */
+//   updateDimensions() {
+//     if(window.innerWidth < 500) {
+//       this.setState({ width: 450, height: 102 });
+//     } else {
+//       let update_width  = window.innerWidth-100;
+//       let update_height = Math.round(update_width/4.4);
+//       this.setState({ width: update_width, height: update_height });
+//     }
+//   }
 
-  /**
-   * Add event listener
-   */
-  componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener("resize", this.updateDimensions.bind(this));
-  }
+//   /**
+//    * Add event listener
+//    */
+//   componentDidMount() {
+//     this.updateDimensions();
+//     window.addEventListener("resize", this.updateDimensions.bind(this));
+//   }
 
-  /**
-   * Remove event listener
-   */
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions.bind(this));
-  }
+//   /**
+//    * Remove event listener
+//    */
+//   componentWillUnmount() {
+//     window.removeEventListener("resize", this.updateDimensions.bind(this));
+//   }
 
 
 
--- src/pages/pdf3.js
+++ src/pages/pdf3.js
@@ -1,98 +1,63 @@
-import React from "react"
+import React, { useState } from "react"
 import {Document, Page, pdfjs} from "react-pdf"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import gate from "../../public/storage/pdf/GATE/Bio2019B.pdf"
 import "./pdftest.css"
-import { Component } from "react"
 import {Button, h3} from "bootstrap-react"
-
+import {
+    useWindowSize,
+  } from '@react-hook/window-size'
 
 pdfjs.GlobalWorkerOptions.workerSrc=`//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-// const [maxPage, setNumPages] = useState(null)
 
-
-export default class PdfTest extends Component{
-    constructor(){
-        super();
-        this.state ={
-            pageNumber: 1,
-            maxPage: 20,
-            width:800,
-            height:1020
+export default function Pdf1(){
+    const [width] = useWindowSize({fps:60});
+    const [pageNumber,setPageNumber] = useState(1);
+    const [maxPage,setMaxPage] = useState(20);
+    function nextPage(){
+        if(pageNumber !== maxPage){
+            setPageNumber(pageNumber+1);
         }
     }
-     /**
-   * Calculate & Update state of new dimensions
-   */
-  updateDimensions() {
-    if(window.innerWidth < 500) {
-      this.setState({ width: 450, height: 102 });
-    } else {
-      let update_width  = window.innerWidth-100;
-      let update_height = Math.round(update_width/4.4);
-      this.setState({ width: update_width, height: update_height });
-    }
-  }
-
-  /**
-   * Add event listener
-   */
-  componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener("resize", this.updateDimensions.bind(this));
-  }
-
-  /**
-   * Remove event listener
-   */
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions.bind(this));
-  }
-    nextPage(){
-        if(this.state.pageNumber !== this.state.maxPage){
-            this.setState({pageNumber:this.state.pageNumber+1})
+    function prevPage(){
+        if(pageNumber !== 1){
+            setPageNumber(pageNumber-1);
         }
     }
-    prevPage(){
-        if(this.state.pageNumber !== 1){
-            this.setState({pageNumber: this.state.pageNumber-1})
-        }
+    function onPDFLoad(Page){
+        setMaxPage(Page);
     }
-    onPDFLoad(Page){
-        this.setState({maxPage:Page});
-    }
-    render(){
-        return(
-            <>
-            <div
-        style={{
-        //   margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-          marginTop: '4em',
-        }}>
-        <Layout>
-            <SEO title="pdf Test" />           
 
-            <div className="button" style={{display: "flex"}}>
-                <Button onClick={()=>this.prevPage()} className="prevButton mr-3">Previous  {this.state.pageNumber-1}</Button>
-                <h3>{this.state.pageNumber} of {this.state.maxPage}</h3>
-                <Button onClick={()=>this.nextPage()} className="nextButton ml-3" >Next   {this.state.pageNumber+1}</Button>    
-            </div>
-            <Document file={gate} onContextMenu={(e) => e.preventDefault()} className="pdf-document" onLoadSuccess={(pdf)=>this.onPDFLoad(pdf.numPages)} renderMode="canvas`">
-                <Page pageNumber={this.state.pageNumber} width={this.state.width}/>
-            </Document>
-            <div style={{display:"flex"}}>
-                <Button onClick={()=>this.nextPage()} className="buttonLast"> <h2>Next</h2></Button>
-            </div>
-        </Layout>
-    </div>
-    </>
-        );
-    };
+    return(
+        <>
+        <div
+    style={{
+    //   margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0 1.0875rem 1.45rem`,
+        marginTop: '4em',
+    }}>
+    <Layout>
+        <SEO title="GATE Biotechnology 2020" />           
+
+        <div className="button" style={{display: "flex"}}>
+            <Button onClick={()=>prevPage()} className="prevButton mr-3">Previous  {pageNumber-1}</Button>
+            <h3>{pageNumber} of {maxPage}</h3>
+            <Button onClick={()=>nextPage()} className="nextButton ml-3" >Next   {pageNumber+1}</Button>    
+        </div>
+        <div style={{display: "flex" ,minWidth: "100%"}}>
+        <Document file={gate} onContextMenu={(e) => e.preventDefault()} className="pdf-document" onLoadSuccess={(pdf)=>onPDFLoad(pdf.numPages)} renderMode="canvas`">
+            <Page pageNumber={pageNumber} width={width}/>
+        </Document>
+        </div>
+        <div style={{display:"flex"}}>
+            <Button onClick={()=>nextPage()} className="buttonLast"> <h2>Next</h2></Button>
+        </div>
+    </Layout>
+</div>
+</>
+    );
 }
-
-// export default PdfTest
