0d5fbfd8d02fbebe450ff59f022dd357060c7e50
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2021-02-25 18:27:08 +0530

    blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,13 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: "./src/blog/",
+      },
+      __key: "blog",
+    },
   ],
 }
