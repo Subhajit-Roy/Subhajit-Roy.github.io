a71d981346ae08e90c587441f0d91d1bd4b9ef1b
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2021-04-13 22:15:04 +0530

    file mark removed due to error

--- src/pages/mark.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {MDXProvider} from '@mdx-js/react'
-import {renderToString} from 'react-dom/server'
-
-// export default function Mark(){
-//     <MDXProvider>
-
-//     </MDXProvider>
-// }
